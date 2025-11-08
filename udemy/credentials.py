ec5855a48b00c168eff70934dbaee4e6edfb573b
SreeSaranE <sreesaran1007@gmail.com>
2025-11-08 08:58:14 +0530

    doc: Remove credentials file from tracking

--- udemy/credentials.py
+++
@@ -1,7 +0,0 @@
-from typing import Final
-
-EMAIL: Final[str] = "alienxtemp1@gmail.com"
-EMAIL_PASSWORD : Final[str] = "@Cg3CPEt$edANsX"
-
-TELEGRAM_API: Final[str] = "7766454350:AAGFdI9IE0G5aqBobZWx8ZN6my_nTYJN4I8"
-TELEGRAM_USERNAME: Final[str] = '@alienX10_bot'
